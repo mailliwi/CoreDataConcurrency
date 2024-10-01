65256500670ade84b6351ce05229753dd1b3f317
William Dupont <williamd.contact@gmail.com>
2024-10-01 16:32:57 +0100

    Initial Commit

---
+++ CoreDataConcurrencyExploration/CoreDataConcurrencyExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataConcurrencyExplorationApp.swift
+//  CoreDataConcurrencyExploration
+//
+//  Created by William Dupont on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataConcurrencyExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
