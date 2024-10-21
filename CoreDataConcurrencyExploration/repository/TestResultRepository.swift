2d6411f140f22dc5a4a782689590d3bb9ca5bc6f
William Dupont <william@ineqe.com>
2024-10-21 12:10:21 +0100

    Try implementing solution with custom global actor and executor

--- CoreDataConcurrencyExploration/repository/TestResultRepository.swift
+++ CoreDataConcurrencyExploration/repository/TestResultRepository.swift
@@ -16,12 +16,13 @@ actor TestResultRepository {
         self.networkService = TestResultsNetworkService()
     }
     
+    @MOCActor
     func fetchTestResults() async throws -> [TestResult] {
         let localTestResults: [TestResult] = try await fetchLocalTestResults()
         await delegate?.emitLocal(localTestResults)
         
         let remoteTestResults: [TestResult] = try await fetchRemoteTestResults()
-        try await createOrUpdateTestResults(remoteTestResults)
+        try createOrUpdateTestResults(remoteTestResults)
         
         let upToDateLocalTestResults: [TestResult] = try await fetchLocalTestResults()
         await delegate?.emitLocal(upToDateLocalTestResults)
@@ -48,121 +49,256 @@ actor TestResultRepository {
         return remoteTestResults
     }
     
-    func createOrUpdateTestResults(_ testResults: [TestResult]) async throws {
+//    func createOrUpdateTestResults(_ testResults: [TestResult]) async throws {
+//        // 1. Get IDs
+//        let testResultsIDs = testResults.compactMap { $0.id }
+//        let backgroundContext = DataController.shared.container.newBackgroundContext()
+//        let fetchRequest = TestResultEntity.fetchRequest()
+//
+//        // 2.Get entities from local that match the IDs
+//        for testResultID in testResultsIDs {
+//            fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
+//
+//            try await backgroundContext.perform {
+//                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+//
+//                // 3. Update entities for found IDs
+//                if let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) {
+//                    let existingTestResult: TestResult = TestResult(from: entity)
+//                    Task { try await self.updateTestResult(existingTestResult) }
+//
+//                // 4. Create new entities for not found IDs
+//                } else {
+//                    if let newTestResult: TestResult = testResults.first(where: { $0.id == testResultID }) {
+//                        Task { try await self.createTestResult(newTestResult) }
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
+    @MOCActor
+    func createOrUpdateTestResults(_ testResults: [TestResult]) throws {
         // 1. Get IDs
-        let testResultsIDs = testResults.compactMap { $0.id }
-        let backgroundContext = DataController.shared.container.newBackgroundContext()
+        let testResultIDs = testResults.compactMap { $0.id }
+        let backgroundContext = MOCActor.sharedMOC
         let fetchRequest = TestResultEntity.fetchRequest()
         
         // 2.Get entities from local that match the IDs
-        for testResultID in testResultsIDs {
+        for testResultID in testResultIDs {
             fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
             
-            try await backgroundContext.perform {
-                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+            let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+            
+            // 3. Update entities for found IDs
+            if let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) {
+                let existingTestResult: TestResult = TestResult(from: entity)
+                Task { try self.updateTestResult(existingTestResult) }
                 
-                // 3. Update entities for found IDs
-                if let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) {
-                    let existingTestResult: TestResult = TestResult(from: entity)
-                    Task { try await self.updateTestResult(existingTestResult) }
-                    
-                // 4. Create new entities for not found IDs
-                } else {
-                    if let newTestResult: TestResult = testResults.first(where: { $0.id == testResultID }) {
-                        Task { try await self.createTestResult(newTestResult) }
-                    }
+            // 4. Create new entities for not found IDs
+            } else {
+                if let newTestResult: TestResult = testResults.first(where: { $0.id == testResultID }) {
+                    Task { try self.createTestResult(newTestResult) }
                 }
             }
         }
     }
     
-    func createTestResult(_ testResult: TestResult) async throws {
-        let delegate = await self.delegate
-        let backgroundContext = DataController.shared.container.newBackgroundContext()
+//    func createTestResult(_ testResult: TestResult) async throws {
+//        let backgroundContext = DataController.shared.container.newBackgroundContext()
+//
+//        try await backgroundContext.perform {
+//            do {
+//                let entity = TestResultEntity(context: backgroundContext)
+//
+//                self.setEntity(entity, fromValuesOf: testResult)
+//
+//                try backgroundContext.save()
+//            } catch {
+//                throw error
+//            }
+//
+//            Task { @MainActor in
+//                self.delegate?.emitLocal(try await self.fetchLocalTestResults())
+//            }
+//        }
+//    }
+    
+    @MOCActor
+    func createTestResult(_ testResult: TestResult) throws {
+        let backgroundContext = MOCActor.sharedMOC
         
-        try await backgroundContext.perform {
-            do {
-                let entity = TestResultEntity(context: backgroundContext)
-                
-                self.setEntity(entity, fromValuesOf: testResult)
-                
-                try backgroundContext.save()
-            } catch {
-                throw error
-            }
+        do {
+            let entity: TestResultEntity = TestResultEntity(context: backgroundContext)
+            setEntity(entity, fromValuesOf: testResult)
             
-            Task { @MainActor [delegate] in
-                delegate?.emitLocal(try await self.fetchLocalTestResults())
-            }
+            try backgroundContext.save()
+        } catch {
+            throw error
+        }
+        
+        Task { @MainActor in
+            self.delegate?.emitLocal(try await self.fetchLocalTestResults())
         }
     }
     
-    func updateTestResult(_ testResult: TestResult) async throws {
+//    func updateTestResult(_ testResult: TestResult) async throws {
+//        guard let testResultID = testResult.id else {
+//            throw CoreDataError.noMatchingEntity
+//        }
+//
+//        let backgroundContext = DataController.shared.container.newBackgroundContext()
+//        let fetchRequest = TestResultEntity.fetchRequest()
+//        fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
+//
+//        try await backgroundContext.perform {
+//            do {
+//                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+//                guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
+//                    throw CoreDataError.noMatchingEntity
+//                }
+//
+//                self.setEntity(entity, fromValuesOf: testResult)
+//
+//                try backgroundContext.save()
+//            } catch {
+//                throw error
+//            }
+//
+//            Task { @MainActor in
+//                self.delegate?.emitLocal(try await self.fetchLocalTestResults())
+//            }
+//        }
+//    }
+    
+    @MOCActor
+    func updateTestResult(_ testResult: TestResult) throws {
         guard let testResultID = testResult.id else {
             throw CoreDataError.noMatchingEntity
         }
         
-        let delegate = await self.delegate
-        let backgroundContext = DataController.shared.container.newBackgroundContext()
+        let backgroundContext = MOCActor.sharedMOC
         let fetchRequest = TestResultEntity.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
         
-        try await backgroundContext.perform {
-            do {
-                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
-                guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
-                    throw CoreDataError.noMatchingEntity
-                }
-                
-                self.setEntity(entity, fromValuesOf: testResult)
-                
-                try backgroundContext.save()
-            } catch {
-                throw error
+        do {
+            let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+            guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
+                throw CoreDataError.noMatchingEntity
             }
             
-            Task { @MainActor [delegate] in
-                delegate?.emitLocal(try await self.fetchLocalTestResults())
-            }
+            setEntity(entity, fromValuesOf: testResult)
+            
+            try backgroundContext.save()
+        } catch {
+            throw error
+        }
+        
+        Task { @MainActor in
+            self.delegate?.emitLocal(try await self.fetchLocalTestResults())
         }
     }
     
-    func deleteTestResult(_ testResult: TestResult) async throws {
-        guard let testResultID = testResult.id else {
-            throw CoreDataError.noMatchingEntity
-        }
-        
-        let delegate = await self.delegate
-        let backgroundContext = DataController.shared.container.newBackgroundContext()
-        let fetchRequest = TestResultEntity.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
-        
-        try await backgroundContext.perform {
-            do {
-                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
-                guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
-                    throw CoreDataError.noMatchingEntity
-                }
-                
-                backgroundContext.delete(entity)
-                
-                try backgroundContext.save()
-            } catch {
-                throw error
-            }
-            
-            Task { @MainActor [delegate] in
-                delegate?.emitLocal(try await self.fetchLocalTestResults())
-            }
-        }
-    }
+//    func deleteTestResult(_ testResult: TestResult) async throws {
+//        guard let testResultID = testResult.id else {
+//            throw CoreDataError.noMatchingEntity
+//        }
+//
+//        let backgroundContext = DataController.shared.container.newBackgroundContext()
+//        let fetchRequest = TestResultEntity.fetchRequest()
+//        fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
+//
+//        try await backgroundContext.perform {
+//            do {
+//                let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+//                guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
+//                    throw CoreDataError.noMatchingEntity
+//                }
+//
+//                backgroundContext.delete(entity)
+//
+//                try backgroundContext.save()
+//            } catch {
+//                throw error
+//            }
+//
+//            Task { @MainActor in
+//                self.delegate?.emitLocal(try await self.fetchLocalTestResults())
+//            }
+//        }
+//    }
     
-    nonisolated private func setEntity(_ entity: TestResultEntity, fromValuesOf domainModel: TestResult) {
+    @MOCActor
+    private func setEntity(_ entity: TestResultEntity, fromValuesOf domainModel: TestResult) {
         entity.id = domainModel.id
         entity.course = domainModel.course
         entity.testName = domainModel.testName
         entity.score = Int16(domainModel.score ?? 0)
         entity.isCertified = domainModel.isCertified ?? false
+    }
+    
+    @MOCActor
+    func deleteTestResult(_ testResult: TestResult) throws {
+        guard let testResultID = testResult.id else {
+            throw CoreDataError.noMatchingEntity
+        }
+        
+        let backgroundContext = MOCActor.sharedMOC
+        let fetchRequest = TestResultEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", testResultID as CVarArg)
+        
+        do {
+            let entities: [TestResultEntity] = try backgroundContext.fetch(fetchRequest)
+            guard let entity: TestResultEntity = entities.first(where: { $0.id == testResultID }) else {
+                throw CoreDataError.noMatchingEntity
+            }
+            
+            backgroundContext.delete(entity)
+            
+            try backgroundContext.save()
+        } catch {
+            throw error
+        }
+        
+        Task { @MainActor in
+            self.delegate?.emitLocal(try await self.fetchLocalTestResults())
+        }
+    }
+    
+}
+
+final class MOCExecutor: SerialExecutor {
+    nonisolated(unsafe) let moc: NSManagedObjectContext
+    
+    init(moc: NSManagedObjectContext) {
+        self.moc = moc
+    }
+    
+    func enqueue(_ job: consuming ExecutorJob) {
+        nonisolated(unsafe) var job: ExecutorJob? = job
+        
+        self.moc.perform {
+            job.take()?.runSynchronously(on: self.asUnownedSerialExecutor())
+        }
+    }
+    
+    func isSameExclusiveExecutionContext(other: MOCExecutor) -> Bool {
+        self.moc == other.moc
+    }
+}
+
+@globalActor actor MOCActor: Actor {
+    static var shared: MOCActor { MOCActor(moc: NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)) }
+    
+    nonisolated private let executor: MOCExecutor
+    nonisolated var unownedExecutor: UnownedSerialExecutor { self.executor.asUnownedSerialExecutor() } // Ask why this is needed
+    var moc: NSManagedObjectContext { self.executor.moc } // Ask why this is needed
+    
+    @MOCActor
+    static var sharedMOC: NSManagedObjectContext { self.shared.executor.moc }
+    
+    private init(moc: NSManagedObjectContext) {
+        self.executor = MOCExecutor(moc: moc)
     }
     
 }
